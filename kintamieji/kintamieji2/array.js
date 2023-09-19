d583478b3dfbe062db2688048b440ff13ffb522a
Laima <pileliene.laima@gmail.com>
2023-09-19 19:03:57 +0300

    arraysString

---
+++ kintamieji/kintamieji2/array.js
@@ -0,0 +1,11 @@
+// Kintamieji: array
+
+const fruits = ["apple", "banana", "grapes"];
+const vegetable = ["tomato", "potato", "brocoli"];
+let vegetarianFood = fruits + " " + vegetable;
+vegetable = "egg";
+vegetarianFood = "peach";
+console.log(fruits);
+console.log(fruits.toString());
+console.log(vegetable);
+console.log(vegetarianFood);
