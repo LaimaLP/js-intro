6adb51c6c241ae68d48c1ff5dbb7f9ce654a34db
Laima <pileliene.laima@gmail.com>
2023-09-19 11:38:40 +0300

    numbers with console

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -10,13 +10,15 @@ Inicijavimo budas:
 - let (jei kinta info)
 - var (niekada)
 */
-
+console.log(amzius);
+console.log(pinigine);
 const amzius = 99;
 let pinigine = 0;
 var minusas = -7.56;
 
 const x1 = 1;
 const x2 = 2;
-console.log(amzius);
-x2 = 6;
+const x3 = 3;
+const x4 = 4;
+
 
