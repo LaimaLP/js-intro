39c6c88782b10dcb5c33b6b40746410181e3eef7
Laima <pileliene.laima@gmail.com>
2023-09-19 15:51:55 +0300

    numbersNode

---
+++ kintamieji/kintamieji2/number2.js
@@ -0,0 +1,27 @@
+/*
+NUMBER
+
+- teigiami/ neimiagi
+- sveikieji/ desimtainiai
+- normalus / nenormalus
+Inicijavimo budas:
+- const
+- let
+- var
+*/
+
+const amzius = 99; //priskiriame//
+let pinigine = 0;
+var minusas = -7.56;
+
+console.log(amzius);
+console.log(pinigine);
+console.log(minusas);
+
+const x1 = 1;
+const x2 = 2;
+const x3 = 3;
+const x4 = 4;
+const x5 = 5;
+
+
