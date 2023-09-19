ad842abd96c06b0df8297c4d62ca9365ae6f9259
Laima <pileliene.laima@gmail.com>
2023-09-19 16:12:32 +0300

    string2

---
+++ kintamieji/kintamieji2/string2.js
@@ -0,0 +1,15 @@
+// Kintamieji STRING
+
+const x = "Laima"
+const y = "Pi"
+
+const z = x + " " + y;
+let m = "namas"
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log(m);
+
+m= "kuku";
