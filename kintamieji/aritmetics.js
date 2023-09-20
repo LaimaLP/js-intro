08d43590244adb0cab27ee0370e70767e72d9fc2
Laima <pileliene.laima@gmail.com>
2023-09-20 15:03:22 +0300

    complex'iniai veiksmai

---
+++ kintamieji/aritmetics.js
@@ -0,0 +1,35 @@
+console.log(5+7);
+
+
+// division
+
+console.log(5/3);
+console.log(5/0);
+console.log(-7/3);
+console.log(-5/-3);
+console.log(0/3);
+
+// complex'iniai:
+
+console.log(-10+5-5-(-5));
+console.log(5/0);
+
+console.log(5-6+(-8)-(-15)-(100*2));
+console.log(5-6+(-8)-(-15)-(-100*2));
+
+console.log(5-6+(-8)-(-15)-(100*2));
+console.log(5-6+(-8)-(-15)-(100**2));
+console.log(5-6+(-8)-(-15)-(-100)**2);
+
+console.log(0.5-0.2);
+console.log(0.5-0.25);
+console.log(-0.5-0.2);
+console.log(0.5-0.2);
+console.log(0.2+0.3);
+console.log(0.05-0.002)
+
+
+
+
+
+
