0b897f5f742de2cadd72f3ab827c71ba2b9d13c0
Laima <pileliene.laima@gmail.com>
2023-09-20 13:44:27 +0300

    strings

---
+++ kintamieji/string.js
@@ -0,0 +1,14 @@
+/*
+STRING
+*/
+
+const txt1 = "tekstas 1";
+console.log(txt1);
+
+const txt2 = 'tekstas 2';
+console.log(txt2);
+
+console.log(txt1, txt2);
+
+const txt3 = "labas"
+console.log(txt3);
