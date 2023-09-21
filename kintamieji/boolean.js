56ed07779b8cf212c4b28eab6daf8ed8ae730b1a
Laima <pileliene.laima@gmail.com>
2023-09-21 16:04:45 +0300

    boolean info

---
+++ kintamieji/boolean.js
@@ -0,0 +1,26 @@
+/*
+BOOLEAN - logine reiksme
+
+Galimos reiksmes: true  (1) / false (0);
+
+Operacijos: && (and)
+            || (or)
+
+*/
+
+console.clear();
+
+const arPilnametis = true;
+const arLyja = false;
+
+const isSunnyDay = true;
+const isNight = false;
+
+console.log(isSunnyDay);
+console.log(isNight);
+
+const count = false + false + true + true + true+ false + true;
+console.log('>>>', count);
+
+const a = true & true;
+console.log(a);
