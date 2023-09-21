0ce3c8e8eb4adeabf88f70432683f55b55158caa
Laima <pileliene.laima@gmail.com>
2023-09-21 16:05:06 +0300

    backtick

--- kintamieji/string.js
+++ kintamieji/string.js
@@ -1,7 +1,7 @@
 /*
 STRING
 
-Inicijavimas kabutemis: "", ' '
+Inicijavimas kabutemis: "", ' ', ``
 
 teksto ilgis: 'kintamasis'.length
 */
@@ -65,27 +65,34 @@ rytas,\n\r\
 Lietuva";
 console.log(lrl);
 
+const lrl3 = `Labas
+rytas,
+Lietuva`;
+console.log(lrl3);
+
+
 console.clear();
+
 
 const vardas = 'Chuck';
 console.log(vardas, '=', vardas.length, 'simboliai');
 const pavarde = "Norris"
 
-// const fullname = vardas  + " " + pavarde;
-// console.log(fullname);
+const fullname = vardas  + " " + pavarde;
+console.log(fullname, fullname.length);
 
-// //Labas rytas, Lietuva!
-// const labas = "Labas";
-// const parosMetas = "vakaras";
-// const valstybe = "Lietuva";
+// Labas rytas, Lietuva!
+const labas = "Labas";
+const parosMetas = "vakaras";
+const valstybe = "Lietuva";
 
-// const lrl2 = labas + ' ' + rytas + ','+ lietuva + "!"
-// console.log(lrl2);
+const lrl2 = labas + ' ' + parosMetas + ','+ valstybe + "!"
+console.log(lrl2);
 
-// const lrl3 = `${labas} ${parosMetas}, ${valstybe}!`;
-// console.log(lrl3);
+const lrl4 = `${labas} ${parosMetas}, ${valstybe}!`;
+console.log(lrl4);
 
-console.clear();
+
 
 
 
@@ -94,5 +101,13 @@ const n1 = 5+7;
 console.log(n1);
 
 
-const n2 = 5 / '7';
+const n2 = 5 ** '7';
 console.log(n2);
+const n3 = `5` ** '7';
+console.log(n3);
+const n4 = `5` ** 7;
+console.log(n4);
+
+
+const ll= `labas` - `l`;
+console.log(ll);
