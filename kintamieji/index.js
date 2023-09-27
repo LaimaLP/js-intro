0599f1cc34e94fba8832b19c91fab33f69754557
Laima <pileliene.laima@gmail.com>
2023-09-27 08:21:29 +0300

    duomenu tipai

--- kintamieji/index.js
+++ kintamieji/index.js
@@ -1,9 +1,15 @@
 /*
 KINTAMIEJI
-
+Primityvus duomenu tipai
 - number
 - string (tekstas)
 - boolean (true/false)
+-undefined
+-null
+
+
+Kompleksiniai duomenu tipai:
 - array (masyvas / kolekcija / sarasas / listas / matrica / arejas)
 - object (objektas - kompleksinei informacijai aprasyti)
+
 */
