e1953d1338e6224ca9647fcdfcdf5abd765e6613
Laima <pileliene.laima@gmail.com>
2023-10-08 19:35:57 +0300

    sprendimai

--- palyginimas/codewars.js
+++ palyginimas/codewars.js
@@ -1,16 +1,12 @@
 console.clear();
 
-console.log('labas')
+console.log("labas");
 const fruits = [];
-fruits.push('a:b', 'c:d');
+fruits.push("a:b", "c:d");
 
 console.log(fruits);
-console.log(4**2)
-console.log(`...............`)
-
-
-
-
+console.log(4 ** 2);
+console.log(`...............`);
 
 // const marks = [10, 2, 8, 4, 6];
 // let totalSum = 0;
@@ -23,23 +19,23 @@ console.log(`...............`)
 // console.log(marks, 'average is', average);
 console.clear();
 
-const ages = [16,2, 18, 99, 50];
-ages.sort(function(a, b){return a - b});
-console.log(ages)
-const max = ages.length
+const ages = [16, 2, 18, 99, 50];
+ages.sort(function (a, b) {
+  return a - b;
+});
+console.log(ages);
+const max = ages.length;
 console.log(max);
-console.log(ages.length)
-console.log([ages[0], ages[max], (ages[max]-ages[0])] )
+console.log(ages.length);
+console.log([ages[0], ages[max], ages[max] - ages[0]]);
 console.log(Math.max.apply(null, ages));
 
-
-const one ="labas" 
-const two = "rytas"
-const three = "katin"
-// const 
+const one = "labas";
+const two = "rytas";
+const three = "katin";
+// const
 // for (let i=0; i<one.length; i++)
- console.log(one[0]+two[0]+three[0])
-
+console.log(one[0] + two[0] + three[0]);
 
 // let string = `apple ban`;
 // console.log(string);
@@ -74,9 +70,6 @@ console.clear();
 // console.log(result);
 // console.log(result.split(' '));
 
-
-
-
 console.clear();
 
 let str = ` `;
@@ -85,32 +78,30 @@ let arr = str.split(" ");
 console.log(arr);
 console.log(arr.reverse()); //kodel nepasidare reverse?
 console.log(arr.join(" "));
-console.log('----------');
-
-
+console.log("----------");
 
 // let str = "The greatest victory is that which requires no battle";
 // console.log(str.reverse(' '));
 
 function reverseWords(str) {
-    // Split the string into an array of words
-    const words = str.split(' ');
-  
-    // Reverse the order of the words in the array
-    const reversedWords = words.reverse();
-  
-    // Join the reversed words back into a string with spaces
-    const reversedString = reversedWords.join(' ');
-  
-    return reversedString;
-  }
+  // Split the string into an array of words
+  const words = str.split(" ");
 
- let sarasas=  [22, -6, 32, 82, 9, 25];
- console.log(sarasas);
- dalinasi = sarasas[2]/[2];
- beLiekanos = sarasas[3]%[3];
- console.log(dalinasi);
- console.log(beLiekanos);
+  // Reverse the order of the words in the array
+  const reversedWords = words.reverse();
+
+  // Join the reversed words back into a string with spaces
+  const reversedString = reversedWords.join(" ");
+
+  return reversedString;
+}
+
+let sarasas = [22, -6, 32, 82, 9, 25];
+console.log(sarasas);
+dalinasi = sarasas[2] / [2];
+beLiekanos = sarasas[3] % [3];
+console.log(dalinasi);
+console.log(beLiekanos);
 
 //  const result = [];
 
@@ -122,17 +113,15 @@ function reverseWords(str) {
 
 //   return result;
 
-
 //   function multipleOfIndex(array) {
 //     let result = [];
 //       for (let i=0; i<array.length; i++){
-//           if(array[i]%i===0){  
+//           if(array[i]%i===0){
 //       result.push(array[i]);
 //         }
 //   }
 //   return result;
 //     }
-
 
 //     function contamination(text, char){
 //         if (text || char === " "){
@@ -150,7 +139,7 @@ function reverseWords(str) {
 //           let mutatedText = " ";
 //           for (i=0; i<text.length; i++){
 //             mutatedText += char;
-//           } return mutatedText; 
+//           } return mutatedText;
 //         }else {
 //           return " ";
 //           }
@@ -162,12 +151,11 @@ function reverseWords(str) {
 //               } return `found the needle at position [i]`
 //             }
 
-
 //             function findNeedle(haystack) {
 //                 for (let i = 0; i<haystack.length; i++){
 //                if(haystack[i] === "needle"){
 //                    return "found the needle at position " + [i];
-//                 } 
+//                 }
 //                   }
 //                 }
 
@@ -181,53 +169,273 @@ function reverseWords(str) {
 
 //                 }
 
-
-                
-                  
 //                      function validateHello(greetings) {
-                    //     greetings = [ "hello", "ciao", "salut", "hallo",  "hola", "ahoj", "czesc"]
-                    //     let string;
-                    //     for (i=0; i<greetings.length; i++){
-                    //       if(string == greetings[i]){
-                    //     return true;
-                    //         }
-                    //         } return false;
-                    //   }
+//     greetings = [ "hello", "ciao", "salut", "hallo",  "hola", "ahoj", "czesc"]
+//     let string;
+//     for (i=0; i<greetings.length; i++){
+//       if(string == greetings[i]){
+//     return true;
+//         }
+//         } return false;
+//   }
 
-                    //   function validateHello(greetings) {
-                    //     greetings = [ "hello", "ciao", "salut", "hallo",  "hola", "ahoj", "czesc"]
-                    //     let string;
-                    //     for (i=0; i<greetings.length; i++){
-                    //       if(string == greetings[i]){
-                    //     return true;
-                    //         }else{
-                    //     return false;
-                    //   }
-                    // }}
+//   function validateHello(greetings) {
+//     greetings = [ "hello", "ciao", "salut", "hallo",  "hola", "ahoj", "czesc"]
+//     let string;
+//     for (i=0; i<greetings.length; i++){
+//       if(string == greetings[i]){
+//     return true;
+//         }else{
+//     return false;
+//   }
+// }}
 
+// function validateHello(greetings) {
 
-                    // function validateHello(greetings) {
-  
-                    //     let input = greetings.toLowerCase();
-                    //       if(input.includes("hello")){
-                    //          return true;
-                    //        }else if (input.includes("ciao")){
-                    //         return true;
-                    //          }else if (input.includes("salut")){
-                    //         return true;
-                    //          }else if (input.includes("hallo")){
-                    //         return true;
-                    //         }else if (input.includes("hola")){
-                    //         return true;
-                    //          }else if (input.includes("ahoj")){
-                    //         return true;
-                    //             }else if (input.includes("czesc")){
-                    //         return true;
-                    //       }else{
-                    //         return false;
-                    //       }
-                    //       }
+//     let input = greetings.toLowerCase();
+//       if(input.includes("hello")){
+//          return true;
+//        }else if (input.includes("ciao")){
+//         return true;
+//          }else if (input.includes("salut")){
+//         return true;
+//          }else if (input.includes("hallo")){
+//         return true;
+//         }else if (input.includes("hola")){
+//         return true;
+//          }else if (input.includes("ahoj")){
+//         return true;
+//             }else if (input.includes("czesc")){
+//         return true;
+//       }else{
+//         return false;
+//       }
+//       }
 console.clear();
- let items = [];
- items.push({a:"b", c:"d"});
-   console.log(items)
+let items = [];
+items.push({ a: "b", c: "d" });
+console.log(items);
+
+let arr2 = [1, 2, 3, 4, 5, 6, 10];
+let sum = 15;
+let kiekis = 0;
+for (i = 0; i < arr2.length; i++) {
+  sum += arr2[i];
+  kiekis++;
+}
+console.log(sum, kiekis);
+console.log(sum / kiekis);
+console.log(sum / arr2.length);
+
+
+
+// function betterThanAverage(classPoints, yourPoints) {
+//   let allPoints = classPoints.concat(yourPoints);
+
+//   let allPointsSum = 0;
+//   for (i = 0; i < allPoints.legth; i++) {
+//     allPointsSum + allPoints[i];
+//   }
+//   let allAverage = allPointsSum / allPoints.legth;
+
+//   let myPointsSum = 0;
+//   for (i = 0; i < yourPoints.legth; i++)
+//    myPointsSum + yourPoints[i];
+// }
+// let yourAverage = myPointsSum / yourPoints.legth;
+// if (yourAverage > allAverage) {
+//   return true;
+// } else {
+//   return false;
+// }
+
+
+
+
+
+let  n = 5;
+let reiksme = 5;
+let arr3 = [];
+console.log(n)
+for(i=n; i>0; i--){
+  arr3.push(i)
+}
+console.log(arr3)
+
+let x = [3,2,5]
+console.log(x)
+
+let arr5=[]
+for(let i=0; i<x.length; i++){
+arr5.push(x[i]*2)
+}
+console.log(arr5)
+
+
+
+console.log('----')
+let arr6 = ["t", "b", "h"]
+console.log(arr6[0])
+
+let arr7=[]
+console.log(arr6)
+
+console.log((arr6.pop()))
+console.log((arr6.shift()))
+console.log(arr6)
+
+console.log(arr6.pop())
+console.log(arr6)
+
+console.log(arr6.pop())
+console.log(arr6)
+let zodis = 'labas'
+console.log(zodis.length)
+let skaicius = -2;
+console.log(skaicius.toString())
+console.log(skaicius.toString().length)
+console.log(skaicius.toString().split(''))
+console.log(skaicius.toString().length)
+
+
+
+
+
+// let PliusH = arr7.push(arr6.pop())
+// console.log(arr7)
+// console.log(arr6.pop())
+// console.log(arr6.push(arr))
+
+console.clear();
+function splitAndMerge(string, separator) {
+  
+  let words  = string.split(" ");
+  console.log("words: ", words);
+  
+  for(let i=0; i<words.length; i++){
+      words[i]=words[i].split("").join(separator)
+        }
+  console.log("words2: ", words);
+  console.log("words2: ", words.join(" "));
+  
+}
+splitAndMerge("My name is John","-")
+
+let vienas = "labai labas,   vakaras tau, sakau, jhkjh, hjhjkj "
+console.log(vienas)
+console.log(vienas.split(',', 3))
+
+let vienas2 = "a a"
+console.log(vienas2)
+console.log(vienas2.split('x'))
+console.log(''.split('x'))
+console.log(vienas2.split('x')+(''.split('x')));
+console.log("--------")
+
+let tuscia = ''.split('x');
+console.log(tuscia)
+let vienasA= vienas2.split('')
+console.log(vienasA)
+console.log(vienasA.concat(tuscia))
+
+
+console.log("--------")
+
+
+
+
+
+const pvz = 'Labas rytas suraitytas vistos koja parasytas';
+console.log(pvz.split('x'));
+console.log(pvz.split('a'));
+console.log(pvz.split(' '));
+
+const pvz2 = 'vasara';
+const pvz2PoSplit = pvz2.split('a');
+console.log(pvz2PoSplit);
+
+console.log('sasasasas'.split('a'));
+console.log('sasasasas'.split('s'));
+// //           |       |         
+
+console.log('asd'.split('x'));
+console.log('x'.split('x'));
+console.log('asd'.split('asd'));
+console.log('labas'.split(''));
+console.log(''.split('x'));
+console.log(''.split(''));
+console.log("--------------------------")
+
+console.log("boolean")
+
+console.log("--------------------------")
+ let tekstas = "labas?";
+ let tekstas2 = "labukas";
+ console.log(tekstas)
+ let masyvas=["nu"]
+ console.log(masyvas)
+
+ console.log(masyvas.push(tekstas))
+ console.log(masyvas)
+
+ console.log(masyvas.push(tekstas2))
+ console.log(masyvas);
+ console.log("--------------------------")
+console.log(tekstas.slice(0,-1))
+console.log("--------------------------")
+console.clear();
+let mark = "!";
+let text = "str!ing!";
+console.log(text.replaceAll("!", ""))
+
+
+
+// https://www.codewars.com/kata/56d93f249c844788bc000002/train/javascript
+// function testit(s) {
+//   // Split the input string into words
+//   const words = s.split(" ");
+
+//   // Iterate through each word
+//   const result = words.map((word) => {
+//     // If the word has more than one letter, modify it
+//     if (word.length > 1) {
+//       const lastIndex = word.length - 1;
+//       const uppercaseLastLetter = word[lastIndex].toUpperCase();
+//       return word.slice(0, lastIndex) + uppercaseLastLetter;
+//     } else {
+//       // If the word has only one letter, keep it as is
+//       return word;
+//     }
+//   });
+
+//   // Join the modified words back into a sentence
+//   return result.join(" ");
+// }
+
+// // Test cases
+// Test.assertSimilar(testit(""), "", "");
+// Test.assertSimilar(testit("a"), "A", "");
+// Test.assertSimilar(testit("b"), "B", "");
+// Test.assertSimilar(testit("a a"), "A A", "");
+// Test.assertSimilar(testit("a b"), "a B", "");
+// Test.assertSimilar(testit("a b c"), "a B C", "");
+// Test.assertSimilar(testit("acd dca"), "acD dcA", ""); // New test case
+// // Add more test cases if needed
+
+// console.clear();
+// console.log("--------")
+
+console.clear();
+ let myArr= ["codewars", "flick", "code", "wars"];
+ console.log(myArr);
+ 
+ let skaiciukas = 37.5;
+ let skaiciukasToString = skaiciukas.toString()
+ 
+ console.log(skaiciukas.toString().length)
+
+ console.log(skaiciukasToString.indexOf("."))
+
+
+
+
