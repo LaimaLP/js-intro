9cd47e57eefd4deca54b2696b37398ad5f7f157b
Laima <pileliene.laima@gmail.com>
2023-10-10 15:07:48 +0300

    sprendimas

--- uzd/mentoriaus1.js
+++ uzd/mentoriaus1.js
@@ -9,15 +9,14 @@
 // 3. Sukurkite true false kintamąjį „markHigherBMI“ su informacija apie ar Marko KMI didesnis nei Jono.
  
 // Bandymo duomenys:
-
 // § 1 duomenys: sveria 78 kg ir yra 1,69 m ūgio. Jonas sveria 92 kg ir yra 1,95 ūgio
-
 // m aukščio.
-
 // § 2 duomenys: sveria 95 kg ir yra 1,88 m ūgio. Jonas sveria 85 kg ir yra 1,76 ūgio
-
 // m aukščio.
+
 console.clear();
+console.clear();
+
 // 1. Išsaugokite Marko ir Jono masę ir ūgį kintamaisiais:
 let markoUgis = 1.69;
 let markoSvoris = 78;
@@ -30,32 +29,41 @@ let jonoUgis2 = 1.76;
 let jonoSvoris2 = 85;
 
 // 2. Apskaičiuokite abiejų jų KMI naudodami formulę 
+console.log("-------------------------")
+function KMI(ugis, svoris){
+    return +(svoris/(ugis**2)).toFixed(2);
+ }
+ console.log('Marko KMI::::::: ', KMI(markoUgis, markoSvoris))
 
 let markoKMI = markoSvoris/markoUgis**2;
-console.log(markoKMI)
+console.log("Marko KMI: ", markoKMI.toFixed(2))
 let jonoKMI = jonoSvoris/jonoUgis**2;
-console.log(jonoKMI)
+console.log("Jono KMI: ",jonoKMI.toFixed(2))
 
 let markoKMI2 = markoSvoris2/markoUgis2**2;
-console.log(markoKMI2)
+console.log("Marko2 KMI: ",markoKMI2.toFixed(2))
 let jonoKMI2 = jonoSvoris2/jonoUgis2**2;
-console.log(jonoKMI2)
+console.log("Jono2 KMI: ",jonoKMI2.toFixed(2))
+
+if(markoKMI>jonoKMI){
+console.log("markHigher - true")
+   
+
+}
+
 
 // 3. Sukurkite true false kintamąjį „markHigherBMI“ su informacija apie ar Marko KMI didesnis nei Jono.
 let atsakymas = markoKMI>jonoKMI;
 let atsakymas2 = markoKMI2>jonoKMI2;
 
 
-// 2 uzuoduotis
+console.log(atsakymas, `Marko KMI (${markoKMI.toFixed(2)}) didesnis nei Jono KMI (${jonoKMI.toFixed(2)}!)`);
+console.log(atsakymas2, `Jono KMI2 (${jonoKMI2.toFixed(2)}) yra didesnis nei Marko2 KMI !(${markoKMI2.toFixed(2)})`);
 
-console.log(atsakymas, "Marko KMI didesnis nei Jono!");
-console.log(atsakymas2, "Jono KMI yra didesnis nei Marko!");
 
-console.log(atsakymas, `Marko KMI (${markoKMI}) didesnis nei Jono KMI (${jonoKMI}!)`);
-console.log(atsakymas2, `Jono KMI (${jonoKMI2}) yra didesnis nei Marko KMI !(${markoKMI2})`);
-
+///////////////////////////////////////////////////////////////
 //3 uzduotis
-
+console.log('-----------------------------------------------------------')
 
 // let delfinuVidurkis = (delfinuTaskai[0]+delfinuTaskai[1]+delfinuTaskai[2])/delfinuTaskai.length
 // let koaluVidurkis = (koalosTaskai[0]+koalosTaskai[1]+koalosTaskai[2])/koalosTaskai.length
@@ -69,62 +77,49 @@ console.log(atsakymas2, `Jono KMI (${jonoKMI2}) yra didesnis nei Marko KMI !(${m
 let delfinuTaskai = [96, 108, 89];
 let koalosTaskai = [88, 91, 110];
 
-let delfinuSum = 0;
-let delfinuCount = 0;
+let delfinuBalai = [97, 112, 101];
+let koalosBalai = [109, 95, 123];
 
-let koalosSum = 0;
-let koalosCount = 0;
+function getAverage(taskai){
+   let sum = 0;
 
-
-// let delfinuBalai = [97, 112, 101];
-// let koalosBalai = [109, 95, 123];
-// function whoseWin(delfinuTaskai, koalosTaskai){
-   
-    for(let i=0; i<delfinuTaskai.length; i++){
-         delfinuSum+= delfinuTaskai[i]
-        delfinuCount ++
+       for(let i=0; i<taskai.length; i++){
+         sum+= taskai[i]
     }
-   
-     for(let i=0; i<koalosTaskai.length; i++){
-        koalosSum+= koalosTaskai[i]
-        koalosCount++
-        console.log(koalosSum)
-    } 
-// }
-        const  delfinuAverage = delfinuSum/delfinuCount
-        console.log((delfinuAverage), "Delfinu vidurkis")
-        const koalosAverage = koalosSum/koalosCount
-      console.log((koalosAverage), "Koalos vidurkis")
+        return   Math.round(sum/taskai.length) 
+}       
+
+//delfinu vidurkis po pirmu rungtyniu
+ const delfinu1Vidurkis = getAverage(delfinuTaskai);
+ console.log(delfinu1Vidurkis)
+ console.log("Delfinu1 vidurkis" , getAverage(delfinuTaskai)) 
+//koalos vidurkis po pirmu rungtyniu
+ const koalos1Vidurkis = getAverage(koalosTaskai)
+ console.log(koalos1Vidurkis)
+ console.log("Koalos1 vidurkis", getAverage(koalosTaskai))
+
+ //delfinu ir koalu vidurkis po antru rungtyniu
+ console.log("Delfinu2 vidurkis", getAverage(delfinuBalai))
+ console.log("Koalos2 vidurkis", getAverage(koalosBalai))
 
 
-    // if(koalosAverage>delfinuAverage){
-    //     return "Koalos laimejo"
-    // }else{
-    //     "Delfinai laimejo"
-    // }
-// }
+ function getWinner(delfinai, koalos){
+  if(delfinai>koalos && delfinai>100){
+    return "delfinai laimejo"
+  }else if(delfinai<koalos &&koalos>100){
+    return "koalos laimejo"
+  }else if(delfinai>100 && koalos>100&& delfinai==koalos){
+    return "lygiosios"
+  }else{
+    return "niekas nelaimejo"
+  }
+}
 
-// console.log(whoseWin(delfinuBalai, koalosBalai));
+console.log("Po pirmu rungtyniu:", getWinner(delfinu1Vidurkis, koalos1Vidurkis))
+console.log("Po antru rungtyniu:", getWinner(getAverage(delfinuBalai), getAverage(koalosBalai)))
 
 
 console.log('labas')
-
-// let lyginu = delfinuVidurkis>koaluVidurkis
-
-// function kienoDidesnisVidurkis (delfinuTaskai, koalosTaskai){
-//     let delfinuTaskai = [96, 108, 89];
-//     let koalosTaskai = [88, 91, 110];
-//     let delfinuVidurkis = (delfinuTaskai[0]+delfinuTaskai[1]+delfinuTaskai[2])/delfinuTaskai.length
-//     let koaluVidurkis = (koalosTaskai[0]+koalosTaskai[1]+koalosTaskai[2])/koalosTaskai.length
-
-
-//      if(delfinuVidurkis>koaluVidurkis){
-//          console.log("Delfinai laimejo");
-//     }else if(koaluVidurkis>delfinuVidurkis){
-//         console.log("Koalos laimejo");
-// }
-
-// }
 
 
 
