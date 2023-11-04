83b5348dbad55424f5fb6cf9240a9e7b2e15ca45
Laima <pileliene.laima@gmail.com>
2023-11-04 16:48:28 +0200

    array loops

--- ciklas/object-loops.js
+++ ciklas/object-loops.js
@@ -5,3 +5,27 @@ const user = {
     age: 99,
     isMarried: true,
 };
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.isMarried)
+console.log(user["name"])
+console.log(user["age"])
+console.log(user["isMarried"])
+
+const userKeys = Object.keys(user);
+console.log(userKeys)
+
+for(let i=0; i<userKeys.length; i++){
+    const key = userKeys[i]
+    const value = user[key];
+    console.log(value)
+}
+
+// FOR-IN
+
+for(const key in user){
+    const value = user[key]
+    console.log(value)
+
+}
