530ae3b43c04dd79e9b55720b8df442faac4e3f3
Laima <pileliene.laima@gmail.com>
2023-11-06 20:29:39 +0200

    destructuring

---
+++ kintamieji/destruct.js
@@ -0,0 +1,45 @@
+console.clear();
+
+const marks = [10, 2, 8, 4, 6];
+const mark1 = marks[0];
+const mark2 = marks[0];
+const mark3 = marks[0];
+const otherMarks = [marks[3], marks[4]];
+
+console.log(mark1, mark2, mark3, otherMarks);
+
+const [m1, m2, m3, ...otherMs] = marks;
+
+console.log(m1, m2, m3, otherMs);
+
+const user = {
+  name: "Jonas",
+  age: 99,
+  isMarried: true,
+  children: [
+    { name: "Maryte", age: 88 },
+    { name: "Petras", age: 77 },
+  ],
+};
+
+// const name = user.name;
+// const age = user.age;
+// const isMarried = user.isMarried;
+
+// console.log(name, age, isMarried)
+
+
+//kas importuojama
+const { isMarried, age, children } = user;
+
+console.log(isMarried, age, children);
+console.log(children)
+
+const zip = 13245;
+const street = "Gedemino g.";
+const houseNr = 7;
+
+
+//kas eksportuojama
+const address = { zip, street, houseNr }
+console.log(address)
