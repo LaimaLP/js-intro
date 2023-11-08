7e730e4e08ce077bd8582cecc81a8092dd2d96f2
Laima <pileliene.laima@gmail.com>
2023-11-09 01:04:45 +0200

    spread

---
+++ method/spread.js
@@ -0,0 +1,28 @@
+console.clear()
+
+const a = [1, 2, 3];
+
+const b = [...a];
+a[0]= 111;
+const c = [...a, ...a]
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+const user = {
+    name: 'Jonas',
+    age: 99,
+    isMarried: true,
+};
+
+const user2 = {
+    ...user,
+    favoriteColor: 'red',
+    name: 'Maryte',
+};
+
+user.age = 50;
+
+console.log(user);
+console.log(user2);
