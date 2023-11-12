2e1872763490b8d3ae0155aa3927272d282b8c35
Laima <pileliene.laima@gmail.com>
2023-11-13 00:28:33 +0200

    rick roll uzdaviniai

--- uzd/rick-roll.js
+++ uzd/rick-roll.js
@@ -65,74 +65,179 @@ Never gonna make you cry
 Never gonna say goodbye
 Never gonna tell a lie and hurt you`;
 
-// Rasti ne tuščių eilučių kiekį;
-// Rasti žodžių kiekį;
-// Rasti ilgiausia eilutę ir gražinti jos tekstą;
-// Rasti ilgiausios eilutės raidžių kiekį;
-// Rasti dažniausiai pasikartojančia raidę (nepriklausomai ar ji didžioji, ar mažoji);
-// Rasti dažniausiai pasikartojantį žodį (nepriklausomai ar jis su didžiosiomis, ar mažosiomis);
-// Rasti unikalių raidžių kiekį;
-// Rasti unikalių žodžių kiekį;
-// Rasti žodžių su apostrofu kiekį;
+// 1 Rasti ne tuščių eilučių kiekį;
+// 2 Rasti žodžių kiekį;
+// 3 Rasti ilgiausia eilutę ir gražinti jos tekstą;
+// 4 Rasti ilgiausios eilutės raidžių kiekį;
+// 5 Rasti dažniausiai pasikartojančia raidę (nepriklausomai ar ji didžioji, ar mažoji);
+// 6 Rasti dažniausiai pasikartojantį žodį (nepriklausomai ar jis su didžiosiomis, ar mažosiomis);
+// 7 Rasti unikalių raidžių kiekį;
+// 8 Rasti unikalių žodžių kiekį;
+// 9 Rasti žodžių su apostrofu kiekį;
 console.clear();
 
-// const eilutes = text.split("\n");
-// console.log(eilutes);
-// console.log("ilgis", eilutes.length);
+// 1 Rasti ne tuščių eilučių kiekį;
 
-console.log("----------------");
+
+
+
 let rows;
-function emptyRows(text) {
+function nonEmptyRows(text) {
   rows = text.split("\n");
-  //   console.log("rows", rows);
-
-  let allLength = rows.length;
 
   let count = 0;
-  for (let i = 0; i < allLength; i++) {
+  for (let i = 0; i < rows.length; i++) {
     if (rows[i].length !== 0) {
       count++;
     }
   }
   return count;
 }
-emptyRows(text);
-console.log("count", emptyRows(text));
 
-let beN = text.replaceAll("\n", " ").split(" ");
+console.log("1. nonEmptyRows:", nonEmptyRows(text));
 
-console.log("beN:", beN.length);
+// 2 Rasti žodžių kiekį;
+
+let textWithoutSymbolsArr = text
+  .replaceAll("\n", " ")
+  .replaceAll("'", "")
+  .replaceAll("(", "")
+  .replaceAll(")", "")
+  .replaceAll(",", "")
+  .toLowerCase()
+  .split(" "); 
 
 function kiekZodziu(text) {
-  let beN = text.replaceAll("\n", " ").split(" ");
-  let count = 0;
 
-  for (i = 0; i < beN.length; i++) {
-    if (beN[i] === "") {
-      count++;
+  let emptyCount = 0;
+
+  for (i = 0; i < textWithoutSymbolsArr.length; i++) {
+    if (textWithoutSymbolsArr[i] === "") {
+      emptyCount++;
     }
   }
-  return beN.length - count;
+  return textWithoutSymbolsArr.length - emptyCount;
 }
-console.log("kiekZodziu", kiekZodziu(text));
+console.log("2. word count", kiekZodziu(text));
 
-// console.log(rows)
+// 3 Rasti ilgiausia eilutę ir gražinti jos tekstą;
 
-function longestRow(text) {
-  rows = text.split("\n");
+let longest;
+function longestRow(rows) {
+  longest = "";
 
-  let longest = rows[0].length;
-
-  for (let i = 1; i < rows.length; i++) {
-    if (rows[i].length > longest) {
-      longest = rows[i].length;
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].length > longest.length) {
+      longest = rows[i];
     }
   }
   return longest;
-  // console.log(rows)
 }
 
-console.log("ats:", longestRow(text));
+console.log("3. Longest row text:", longestRow(rows));
+
+// 4 Rasti ilgiausios eilutės raidžių kiekį;
+let withoutSymbol;
+function wordsInLongest(longest) {
+  withoutSymbol = longest
+    .replaceAll(" ", "")
+    .replaceAll("'", "")
+    .replaceAll("(", "")
+    .replaceAll(")", "")
+    .replaceAll(",", "");
+  return withoutSymbol.length;
+
+}
+console.log("4. longest row words length:", wordsInLongest(longest));
+
+// 5 Rasti dažniausiai pasikartojančia raidę (nepriklausomai ar ji didžioji, ar mažoji);
+
+// let textWithoutSymbolsArr = text
+//   .replaceAll("\n", " ")
+//   .replaceAll("'", "")
+//   .replaceAll("(", "")
+//   .replaceAll(")", "")
+//   .replaceAll(",", "")
+//   .toLowerCase()
+//   .split(" ");
+
+// console.log("--------------------------textWithoutSymbols", textWithoutSymbolsArr);
+
+const songObjOfLetters = {};
+
+myText = textWithoutSymbolsArr.join("");
+
+// console.log("myText:", myText)
+
+for (const letter of myText) {
+  if (songObjOfLetters[letter]) {
+    songObjOfLetters[letter] += 1;
+  } else {
+    songObjOfLetters[letter] = 1;
+  }
+}
+// console.log("songObjOfLetters:", songObjOfLetters)
+let maxLetter = "";
+let maxCount = 0;
+
+for (const letter in songObjOfLetters) {
+  if (songObjOfLetters[letter] > maxCount) {
+    maxLetter = letter;
+    maxCount = songObjOfLetters[letter];
+  }
+}
+console.log("5.>>>>", maxLetter, maxCount);
+
+// 6 Rasti dažniausiai pasikartojantį žodį (nepriklausomai ar jis su didžiosiomis, ar mažosiomis);
+
+// console.clear();
+// console.log("beN:",beN)
+
+const songObjOfWords = {};
+
+for (word of textWithoutSymbolsArr) {
+  if (songObjOfWords[word]) {
+    songObjOfWords[word] += 1;
+  } else {
+    songObjOfWords[word] = 1;
+  }
+}
+
+// console.log("songObjOfWords", songObjOfWords)
+
+let maxWord = "";
+let maxCountWord = 0;
+for (const word in songObjOfWords) {
+  if (songObjOfWords[word] > maxCountWord) {
+    maxWord = word;
+    maxCountWord = songObjOfWords[word];
+  }
+}
+console.log("6.>>>>", maxWord, maxCountWord);
+
+// 7 Rasti unikalių raidžių kiekį;
+
+// console.log("songObj:",songObj)
+const uniqueLettersCount = Object.keys(songObjOfLetters).length;
+
+console.log("7. uniqueLettersCount", uniqueLettersCount);
+
+//8
+const uniqueWordsCount = Object.keys(songObjOfWords).length;
+
+console.log("8. uniqueWordsCount:", uniqueWordsCount);
+
+function howMuchApostrophe(text) {
+  let apostropheCount = 0;
+  for(const words of text){
+  if (words.includes("'")) {
+    apostropheCount++;
+  }
+}
+  return apostropheCount
+}
+
+console.log("8. apostropheCount:", howMuchApostrophe(text));
 
 // Non empty lines -> 54
 // Word count -> 353
